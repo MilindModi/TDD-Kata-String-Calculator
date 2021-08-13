7d8719e7d7c8db97b5f52da3d3ee3cff66e91eb7
Milind Modi <milindmodi10@gmail.com>
2021-08-13 12:48:54 +0530

    Test : add empty string testcase

--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -1,2 +1,9 @@
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
 public class StringCalculatorTest {
+    @Test
+    public void addTest(){
+        assertEquals(StringCalculator.add(""),0); //Empty String test case
+    }
 }
