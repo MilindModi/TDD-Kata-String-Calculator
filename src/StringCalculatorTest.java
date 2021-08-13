77b21f8348504afc78fcafe8e8649812275c0fd7
Milind Modi <milindmodi10@gmail.com>
2021-08-13 12:58:02 +0530

    Test: add signle digit testcase

--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -5,5 +5,6 @@ public class StringCalculatorTest {
     @Test
     public void addTest(){
         assertEquals(StringCalculator.add(""),0); //Empty String test case
+        assertEquals(StringCalculator.add("19"),19); //single digit string test case
     }
 }
