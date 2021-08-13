151d3672aa6fc97d5b3cbc3a9f05a2ed1c28f819
Milind Modi <milindmodi10@gmail.com>
2021-08-13 13:10:49 +0530

    Update : Enhanced Testcases with messages

--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -4,9 +4,18 @@ import static org.testng.Assert.*;
 public class StringCalculatorTest {
     @Test
     public void addTest(){
-        assertEquals(StringCalculator.add(""),0); //Empty String test case
-        assertEquals(StringCalculator.add("19"),19); //single digit string test case
-        assertEquals(StringCalculator.add("1,2"),3); //coma(,) separated digit String test case
-        assertEquals(StringCalculator.add("1,2,3"),6); //coma(,) separated digit String test case
+        //Empty String test case
+        assertEquals(StringCalculator.add(""),0,"Empty string testcase failure");
+
+        //single digit string test case
+        assertEquals(StringCalculator.add("19"),19,"Single digit testcase failure");
+
+        //coma(,) separated digit String test case
+        assertEquals(StringCalculator.add("1,2"),3,"two digit coma separated testcase failure");
+
+        //coma(,) separated digit String test case
+        assertEquals(StringCalculator.add("1,2,3"),6,"coma(,) separated digit String testcase failure");
+
+        assertEquals(StringCalculator.add("2,2,5,5"),14,"coma separated digits testcase failure");
     }
 }
