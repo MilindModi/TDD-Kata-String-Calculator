4ec71e20896a997988fe445d54a7bf9f0fee0841
Milind Modi <milindmodi10@gmail.com>
2021-08-13 13:34:50 +0530

    Update : handle custom delimiter in string

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -3,7 +3,14 @@ public class StringCalculator {
         if(numbers.isEmpty()){
             return 0;
         }
-        String []numTokens = numbers.split(",|\n"); //splitting token , as delimiter
+        String separator = getSeparator(numbers);
+
+        if(numbers.startsWith("//")){
+            numbers = numbers.substring(numbers.indexOf("\n") + 1);
+        }
+
+        String []numTokens = numbers.split(separator); //splitting token , as delimiter
+
         if(numTokens.length == 1){
             return Integer.parseInt(numbers);
         }else{
@@ -13,5 +20,15 @@ public class StringCalculator {
             }
             return sum;
         }
+    }
+
+    public static String getSeparator(String number){
+        String separator = "[\n";
+        if(number.startsWith("//")){
+            separator += number.substring(2,number.indexOf("\n")) +"]";
+        }else{
+            separator += ",]";
+        }
+        return separator;
     }
 }
